4ec8aa6d0e0166335ae97189642dd93f36c02e1f
EP\gile <giang.le@episerver.com>
2017-06-13 16:55:32 +0700

    Initial commit

---
+++ src/app/containers/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Popover from '../components/Popover';
+
+class App extends Component {
+  render() {
+    return (
+      <Popover className={`song-heart`}>
+        <i className="icon ion-ios-heart">ra ra</i>
+        <div>
+          <p>Test ok</p>
+        </div>
+      </Popover>
+    );
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require('path');
+var DIST_PATH = path.resolve(__dirname, 'dist');
+var SOURCE_PATH = path.resolve(__dirname, 'src');
+
+module.exports = {
+  entry: SOURCE_PATH + '/app/main.js', // entry file where bundler start the bundling process
+  output: {
+    path: DIST_PATH,
+    filename: 'app.dist.js',
+    publicPath: '/src/app/'
+  },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query:
+          {
+            presets:['react']
+          }
+      }
+    ]
+  }
+}
